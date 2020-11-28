df9c58e6223f779e6d912ec9e42e782e894d4e67
majes <liones2014@mail.ru>
2020-11-28 21:40:00 +0700

    get requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {Movies} from './models/movies';
+import {MovieService} from './services/movie.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'movies';
+export class AppComponent implements OnInit, OnDestroy {
+  constructor(protected movie: MovieService) {
+  }
+
+  subs: Subscription[] = [];
+  trending: Movies | undefined;
+  popular: Movies | undefined;
+  topRated: Movies | undefined;
+  nowPlaying: Movies | undefined;
+  latestMovie: Movies | undefined;
+
+  ngOnInit(): void {
+    this.subs.push(this.movie.getLatestMovie().subscribe(data => {
+      this.latestMovie = data;
+      console.log(data);
+    }));
+    this.subs.push(this.movie.getPopularMovies().subscribe(data => this.popular = data));
+    this.subs.push(this.movie.getTopRated().subscribe(data => this.topRated = data));
+    this.subs.push(this.movie.getNowPlaying().subscribe(data => this.nowPlaying = data));
+
+  }
+
+  ngOnDestroy(): void {
+    this.subs.map(s => s.unsubscribe());
+  }
+
 }
